62a81f266511a9545ecc0ddfa93ac3d526573f25
HANSEOL <nothinonthere@icoud.com>
2023-06-28 16:35:45 +0900

    Initial Commit

---
+++ 2023 summerVac bookcontroll/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2023 summerVac bookcontroll
+//
+//  Created by 한설 on 2023/06/28.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
